913b82e062c573797e3bd7e82ca45b0c9f71bf58
Herman Slatman <hermanslatman@hotmail.com>
2021-01-02 22:16:15 +0100

    Add example TCP layer 4 app with CrowdSec matcher

--- vendor/github.com/mholt/caddy-l4/imports.go
+++ vendor/github.com/mholt/caddy-l4/imports.go
@@ -1,10 +1,10 @@
-// Copyright 2020 Herman Slatman
+// Copyright 2020 Matthew Holt
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,19 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package caddyl4
 
 import (
-	cmd "github.com/caddyserver/caddy/v2/cmd"
-
-	_ "github.com/caddyserver/caddy/v2/modules/standard"
-	_ "github.com/caddyserver/format-encoder"
-
-	_ "github.com/mholt/caddy-l4"
-
-	_ "github.com/hslatman/caddy-crowdsec-bouncer/pkg/crowdsec"
+	// plugging in the standard modules for the layer4 app
+	_ "github.com/mholt/caddy-l4/layer4"
+	_ "github.com/mholt/caddy-l4/modules/l4echo"
+	_ "github.com/mholt/caddy-l4/modules/l4http"
+	_ "github.com/mholt/caddy-l4/modules/l4proxy"
+	_ "github.com/mholt/caddy-l4/modules/l4ssh"
+	_ "github.com/mholt/caddy-l4/modules/l4tee"
+	_ "github.com/mholt/caddy-l4/modules/l4throttle"
+	_ "github.com/mholt/caddy-l4/modules/l4tls"
 )
-
-func main() {
-	cmd.Main()
-}
