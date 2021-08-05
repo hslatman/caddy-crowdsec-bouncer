0a1fdcf22647a70e100425bb959ba3c824337a51
Herman Slatman <hermanslatman@hotmail.com>
2021-08-05 23:38:28 +0200

    Try with imports.go

---
+++ imports.go
@@ -0,0 +1,7 @@
+package crowdsec
+
+import (
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/crowdsec"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/http"
+	_ "github.com/hslatman/caddy-crowdsec-bouncer/layer4"
+)
