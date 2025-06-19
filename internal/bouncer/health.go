7c9f194c0aa495200606aa6e8adcf1418fda1253
Herman Slatman <hermanslatman@hotmail.com>
2025-06-20 01:20:35 +0200

    Use lookup for `127.0.0.255` as a means to "ping" the CrowdSec LAPI
    Apparently the `HeartBeat` service that exists on the CrowdSec API
    client is not meant to be used from bouncers, and resulted in 401s.
    This commit changes the `Ping` implementation to check `127.0.0.255`
    to be blocked, with a successful response indicating a connection
    could be made to the CrowdSec LAPI.

--- internal/bouncer/health.go
+++ internal/bouncer/health.go
@@ -1,15 +1,24 @@
 package bouncer
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
+// Healthy reports whether the [Bouncer]'s current state is considered
+// healthy.
 func (b *Bouncer) Healthy(ctx context.Context) (bool, error) {
-	ok, _, err := b.liveBouncer.APIClient.HeartBeat.Ping(ctx) // TODO: more checks?
-
-	return ok, err
+	return b.Ping(ctx) // TODO: more checks? Make this _not_ depend on (new) external calls?
 }
 
+// Ping "pings" the CrowdSec LAPI to verify a successful connection can be
+// made. It looks up the broadcast IP for the localhost IP address range, which
+// realistically should never be blocked. A successful response thus indicates
+// that a connection could be made.
 func (b *Bouncer) Ping(ctx context.Context) (bool, error) {
-	ok, _, err := b.liveBouncer.APIClient.HeartBeat.Ping(ctx)
+	if _, err := b.liveBouncer.Get("127.0.0.255"); err != nil {
+		return false, fmt.Errorf("failed reaching CrowdSec LAPI: %w", err)
+	}
 
-	return ok, err
+	return true, nil
 }
