e93c427dbb7d86fd2493c0abea2b2937de40b779
Herman Slatman <hermanslatman@hotmail.com>
2025-06-19 21:00:36 +0200

    Cleanup command and Admin API client code

--- internal/command/client.go
+++ internal/command/client.go
@@ -9,108 +9,112 @@ import (
 	"net/netip"
 
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
 	"github.com/hslatman/caddy-crowdsec-bouncer/internal/adminapi"
+	"github.com/hslatman/caddy-crowdsec-bouncer/internal/version"
+)
+
+const (
+	userAgentName = "caddy-crowdsec-cmd"
 )
 
 type adminClient struct {
 	address string
+	headers http.Header
 }
 
-func newAdminClient(address, configFile, configAdapter string) (*adminClient, error) {
-	adminAddress, err := caddycmd.DetermineAdminAPIAddress(address, nil, configFile, configAdapter)
+func newAdminClient(fl caddycmd.Flags) (*adminClient, error) {
+	adminAddress, err := caddycmd.DetermineAdminAPIAddress(fl.String("address"), nil, fl.String("config"), fl.String("adapter"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to determine admin API address: %w", err)
 	}
 
+	userAgentVersion := version.Current()
+	userAgent := userAgentName + "/" + userAgentVersion
+
 	return &adminClient{
 		address: adminAddress,
+		headers: http.Header{
+			"User-Agent": []string{userAgent},
+		},
 	}, nil
 }
 
-func (c *adminClient) Health() (adminapi.HealthResponse, error) {
-	resp, err := caddycmd.AdminAPIRequest(c.address, http.MethodPost, "/crowdsec/health", nil, nil)
+func (c *adminClient) doRequest(path string, body io.Reader) ([]byte, error) {
+	resp, err := caddycmd.AdminAPIRequest(c.address, http.MethodPost, path, c.headers, body)
 	if err != nil {
-		return adminapi.HealthResponse{}, fmt.Errorf("failed getting CrowdSec health: %w", err)
+		return nil, fmt.Errorf("admin API request failed: %w", err)
 	}
 	defer func() { _ = resp.Body.Close() }()
 
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return adminapi.HealthResponse{}, fmt.Errorf("failed reading CrowdSec health: %w", err)
+		return nil, fmt.Errorf("reading admin API response failed: %w", err)
+	}
+
+	return b, nil
+}
+
+func (c *adminClient) Health() (*adminapi.HealthResponse, error) {
+	b, err := c.doRequest("/crowdsec/health", nil)
+	if err != nil {
+		return nil, err
 	}
 
 	var s adminapi.HealthResponse
 	if err := json.Unmarshal(b, &s); err != nil {
-		return adminapi.HealthResponse{}, fmt.Errorf("failed unmarshaling CrowdSec health: %w", err)
+		return nil, fmt.Errorf("failed unmarshaling CrowdSec health: %w", err)
 	}
 
-	return s, nil
+	return &s, nil
 }
 
-func (c *adminClient) Ping() (adminapi.PingResponse, error) {
-	resp, err := caddycmd.AdminAPIRequest(c.address, http.MethodPost, "/crowdsec/ping", nil, nil)
+func (c *adminClient) Ping() (*adminapi.PingResponse, error) {
+	b, err := c.doRequest("/crowdsec/ping", nil)
 	if err != nil {
-		return adminapi.PingResponse{}, fmt.Errorf("failed pinging CrowdSec LAPI: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return adminapi.PingResponse{}, fmt.Errorf("failed reading CrowdSec LAPI ping response: %w", err)
+		return nil, err
 	}
 
 	var p adminapi.PingResponse
 	if err := json.Unmarshal(b, &p); err != nil {
-		return adminapi.PingResponse{}, fmt.Errorf("failed unmarshaling CrowdSec LAPI ping response: %w", err)
+		return nil, fmt.Errorf("failed unmarshaling CrowdSec LAPI ping response: %w", err)
 	}
 
-	return p, nil
+	return &p, nil
 }
 
-func (c *adminClient) Info() (adminapi.InfoResponse, error) {
-	resp, err := caddycmd.AdminAPIRequest(c.address, http.MethodPost, "/crowdsec/info", nil, nil)
+func (c *adminClient) Info() (*adminapi.InfoResponse, error) {
+	b, err := c.doRequest("/crowdsec/info", nil)
 	if err != nil {
-		return adminapi.InfoResponse{}, fmt.Errorf("failed getting CrowdSec info: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return adminapi.InfoResponse{}, fmt.Errorf("failed reading CrowdSec info: %w", err)
+		return nil, err
 	}
 
 	var i adminapi.InfoResponse
 	if err := json.Unmarshal(b, &i); err != nil {
-		return adminapi.InfoResponse{}, fmt.Errorf("failed unmarshaling CrowdSec info: %w", err)
+		return nil, fmt.Errorf("failed unmarshaling CrowdSec info: %w", err)
 	}
 
-	return i, nil
+	return &i, nil
 }
 
-func (c *adminClient) Check(ip netip.Addr, forceLive bool) (adminapi.CheckResponse, error) {
+func (c *adminClient) Check(ip netip.Addr, forceLive bool) (*adminapi.CheckResponse, error) {
 	reqBytes, err := json.Marshal(adminapi.CheckRequest{
 		IP:        ip.String(),
 		ForceLive: forceLive,
 	})
 	if err != nil {
-		return adminapi.CheckResponse{}, fmt.Errorf("failed marshaling CrowdSec check request: %w", err)
+		return nil, fmt.Errorf("failed marshaling CrowdSec check request: %w", err)
 	}
 
-	resp, err := caddycmd.AdminAPIRequest(c.address, http.MethodPost, "/crowdsec/check", nil, bytes.NewReader(reqBytes))
+	b, err := c.doRequest("/crowdsec/info", bytes.NewReader(reqBytes))
 	if err != nil {
-		return adminapi.CheckResponse{}, fmt.Errorf("failed getting CrowdSec check response: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return adminapi.CheckResponse{}, fmt.Errorf("failed reading CrowdSec check response: %w", err)
+		return nil, err
 	}
 
 	var r adminapi.CheckResponse
 	if err := json.Unmarshal(b, &r); err != nil {
-		return adminapi.CheckResponse{}, fmt.Errorf("failed unmarshaling CrowdSec check response: %w", err)
+		return nil, fmt.Errorf("failed unmarshaling CrowdSec check response: %w", err)
 	}
 
-	return r, nil
+	return &r, nil
 }
--- internal/command/command.go
+++ internal/command/command.go
@@ -9,151 +9,147 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 	"github.com/spf13/cobra"
+
+	"github.com/hslatman/caddy-crowdsec-bouncer/internal/version"
 )
 
 func Register() {
 	caddycmd.RegisterCommand(caddycmd.Command{
 		Name:  "crowdsec",
-		Usage: ``, //Usage: "[--config <path> [--adapter <name>]] [--envfile <path>] [--watch] [--pidfile <file>]",
+		Usage: ``,
 		Short: "Commands related to the CrowdSec integration",
 		Long:  ``,
 		CobraFunc: func(cmd *cobra.Command) {
 			statusCmd := &cobra.Command{
 				Use:   "info [--config <path> [--adapter <name>]] [--address <interface>]",
-				Short: "Shows some CrowdSec runtime information",
+				Short: "Shows CrowdSec runtime information",
 				Long:  ``,
 				RunE:  caddycmd.WrapCommandFuncForCobra(cmdInfo),
 			}
-			statusCmd.Flags().StringP("config", "c", "", "Configuration file to use to parse the admin address, if --address is not used")
-			statusCmd.Flags().StringP("adapter", "a", "", "Name of config adapter to apply (when --config is used)")
-			statusCmd.Flags().StringP("address", "", "", "The address to use to reach the admin API endpoint, if not the default")
-			cmd.AddCommand(statusCmd)
-
 			healthCmd := &cobra.Command{
 				Use:   "health [--config <path> [--adapter <name>]] [--address <interface>]",
 				Short: "Checks CrowdSec health",
 				Long:  ``,
 				RunE:  caddycmd.WrapCommandFuncForCobra(cmdHealth),
 			}
-			healthCmd.Flags().StringP("config", "c", "", "Configuration file to use to parse the admin address, if --address is not used")
-			healthCmd.Flags().StringP("adapter", "a", "", "Name of config adapter to apply (when --config is used)")
-			healthCmd.Flags().StringP("address", "", "", "The address to use to reach the admin API endpoint, if not the default")
-			cmd.AddCommand(healthCmd)
-
-			checkCmd := &cobra.Command{
-				Use:   "check <ip> [--config <path> [--adapter <name>]] [--address <interface>]",
-				Short: "Checks an IP",
-				Long:  ``,
-				RunE:  caddycmd.WrapCommandFuncForCobra(cmdCheck),
-			}
-			checkCmd.Flags().StringP("config", "c", "", "Configuration file to use to parse the admin address, if --address is not used")
-			checkCmd.Flags().StringP("adapter", "a", "", "Name of config adapter to apply (when --config is used)")
-			checkCmd.Flags().StringP("address", "", "", "The address to use to reach the admin API endpoint, if not the default")
-			checkCmd.Flags().BoolP("live", "", false, `Force the check to use the "live" bouncer to check the IP`)
-			cmd.AddCommand(checkCmd)
-
 			pingCmd := &cobra.Command{
 				Use:   "ping [--config <path> [--adapter <name>]] [--address <interface>]",
 				Short: "Pings the CrowdSec LAPI endpoint",
 				Long:  ``,
 				RunE:  caddycmd.WrapCommandFuncForCobra(cmdPing),
 			}
-			pingCmd.Flags().StringP("config", "c", "", "Configuration file to use to parse the admin address, if --address is not used")
-			pingCmd.Flags().StringP("adapter", "a", "", "Name of config adapter to apply (when --config is used)")
-			pingCmd.Flags().StringP("address", "", "", "The address to use to reach the admin API endpoint, if not the default")
-			pingCmd.Flags().BoolP("live", "", false, `Force the check to use the "live" bouncer to check the IP`)
+			checkCmd := &cobra.Command{
+				Use:   "check <ip> [--config <path> [--adapter <name>]] [--address <interface>]",
+				Short: "Checks an IP to be banned or not",
+				Long:  ``,
+				RunE:  caddycmd.WrapCommandFuncForCobra(cmdCheck),
+			}
+			checkCmd.Flags().BoolP("live", "", false, `Force the check to use the "live" bouncer to check the IP`)
+
+			// shared flags for all subcommands
+			cmd.PersistentFlags().StringP("config", "c", "", "Configuration file to use to parse the admin address, if --address is not used")
+			cmd.PersistentFlags().StringP("adapter", "a", "", "Name of config adapter to apply (when --config is used)")
+			cmd.PersistentFlags().StringP("address", "", "", "The address to use to reach the admin API endpoint, if not the default")
+
+			// add the subcommands
+			cmd.AddCommand(statusCmd)
+			cmd.AddCommand(healthCmd)
+			cmd.AddCommand(checkCmd)
 			cmd.AddCommand(pingCmd)
+
+			// set version for this command to the CrowdSec module version
+			cmd.Version = version.Current()
 		},
 	})
 }
 
 const (
-	errorCode   = caddy.ExitCodeFailedStartup
-	successCode = caddy.ExitCodeSuccess
+	exitCodeError   = caddy.ExitCodeFailedStartup
+	exitCodeSuccess = caddy.ExitCodeSuccess
 )
 
 func cmdInfo(fl caddycmd.Flags) (int, error) {
-	c, err := newAdminClient(fl.String("address"), fl.String("config"), fl.String("adapter"))
+	c, err := newAdminClient(fl)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed creating Caddy admin client: %w", err)
+		return exitCodeError, err
 	}
 
 	s, err := c.Info()
 	if err != nil {
-		return errorCode, fmt.Errorf("failed getting CrowdSec runtime info: %w", err)
+		return exitCodeError, fmt.Errorf("failed getting CrowdSec runtime info: %w", err)
 	}
 
 	b, err := json.MarshalIndent(s, "", "  ")
 	if err != nil {
-		return errorCode, fmt.Errorf("failed marshaling CrowdSec runtime info: %w", err)
+		return exitCodeError, fmt.Errorf("failed marshaling CrowdSec runtime info: %w", err)
 	}
 
 	fmt.Println(string(b))
 
-	return successCode, nil
+	return exitCodeSuccess, nil
 }
 
 func cmdHealth(fl caddycmd.Flags) (int, error) {
-	c, err := newAdminClient(fl.String("address"), fl.String("config"), fl.String("adapter"))
+	c, err := newAdminClient(fl)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed creating Caddy admin client: %w", err)
+		return exitCodeError, err
 	}
 
 	h, err := c.Health()
 	if err != nil {
-		return errorCode, fmt.Errorf("failed getting CrowdSec runtime health: %w", err)
+		return exitCodeError, fmt.Errorf("failed getting CrowdSec runtime health: %w", err)
 	}
 
 	if !h.Ok {
-		return errorCode, nil
+		return exitCodeError, nil
 	}
 
-	return successCode, nil
+	return exitCodeSuccess, nil
 }
 
 func cmdPing(fl caddycmd.Flags) (int, error) {
-	c, err := newAdminClient(fl.String("address"), fl.String("config"), fl.String("adapter"))
+	c, err := newAdminClient(fl)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed creating Caddy admin client: %w", err)
+		return exitCodeError, err
 	}
 
 	h, err := c.Ping()
 	if err != nil {
-		return errorCode, fmt.Errorf("failed pinging CrowdSec LAPI: %w", err)
+		return exitCodeError, fmt.Errorf("failed pinging CrowdSec LAPI: %w", err)
 	}
 
 	if !h.Ok {
 		fmt.Println("failed")
-		return errorCode, nil
+		return exitCodeError, nil
 	}
 
 	fmt.Println("success")
-	return successCode, nil
+	return exitCodeSuccess, nil
 }
 
 func cmdCheck(fl caddycmd.Flags) (int, error) {
-	c, err := newAdminClient(fl.String("address"), fl.String("config"), fl.String("adapter"))
+	c, err := newAdminClient(fl)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed creating Caddy admin client: %w", err)
+		return exitCodeError, err
 	}
 
 	ipString := fl.Arg(0)
 	if ipString == "" {
-		return errorCode, errors.New("ip required")
+		return exitCodeError, errors.New("ip required")
 	}
 
 	ip, err := netip.ParseAddr(ipString)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed parsing %q as IP address: %w", ipString, err)
+		return exitCodeError, fmt.Errorf("failed parsing %q as IP address: %w", ipString, err)
 	}
 
 	forceLive := fl.Bool("live")
 	h, err := c.Check(ip, forceLive)
 	if err != nil {
-		return errorCode, fmt.Errorf("failed checking IP %q: %w", ip, err)
+		return exitCodeError, fmt.Errorf("failed checking IP %q: %w", ip, err)
 	}
 
 	fmt.Println("blocked:", h.Blocked)
 
-	return successCode, nil
+	return exitCodeSuccess, nil
 }
